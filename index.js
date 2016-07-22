026dacc1cb36d5f244d7409c3c2f08ab1670cdb6
pinky <orochi_alex@hotmail.com>
2016-07-22 11:18:23 -0500

    text boxes added

--- index.js
+++ index.js
@@ -1,3 +1,127 @@
-function test(){
-	alert("alive");
+webComponent = {
+	formValues : [],	
+	responseFields: [],
+	isValidForm: function(){
+		var controller = this;
+		var filter ;
+		filter = $.grep(webComponent.formValues, function(e){ return e.required === true; }); 
+		return filter;
+	},
+	init: function (entidad, tipoPago) {
+		var controller = this;
+		$.ajax({
+			//url: 'http://10.15.3.31:3000/vun/actas_nacimiento/findOne?filter={"where":{"id_estado":"'+entidad+'","id_tipo_pago":'+ tipoPago +'}}',
+			url:'http://localhost:1337/options/1',
+			type: 'GET',
+			dataType: 'json',
+			contentType: 'application/json; charset=UTF-8;',
+		}).done(function(entityFields) {
+			if(entityFields.PagoEnLinea){
+				controller.formValues = entityFields.PagoEnLinea; 
+			}
+			else if (entityFields.PagoReferenciado){
+				controller.formValues = entityFields.PagoReferenciado ;
+			}
+			else{
+				alert("No existen campos")
+			}
+			render( controller.formValues );
+		})
+		.fail(function(e) {
+			alert(JSON.stringify(e));
+		});
+
+		function render(entityFields, canvas){
+			
+			$.each( entityFields, function( index, field ) {
+				validateFieldsClass(field);
+				switch(field.type) {					
+					case "text":
+						var id = field.name + index;
+						var div = getOrCreateDiv(id, field.class);
+						getOrCreateLabel(div,id, field.label);
+						getOrCreateTextInput(div,id, field);
+						break;
+					default: 
+						alert('Default case');
+				}
+			});
+		};
+
+		function getOrCreateDiv(id, clazz){
+			var div = $("#" + id + "div");
+			if(div.length === 0){
+				div = $('<div/>')
+				div.attr("id", id + "div")
+				div.addClass(clazz);
+				$("#container").append(div);
+			}
+			return div;
+		};
+
+		function getOrCreateLabel(div, id, label){
+			var labelObject = $("#" + id + "_label");
+			if(labelObject.length === 0){
+				labelObject = $("<label class='control-label' id='" + id + "_label' for = "+ id + '_input' +" >"+ label + "</label>");
+				div.append(labelObject);
+			}
+			return label;
+		};
+
+		function getOrCreateTextInput(div, id, field){
+			var input = $("#" + id );
+			if(input.length === 0){
+				input = $("<input/>");
+				input.attr("id", id )
+				input.addClass('form-control')
+				input.attr("type", field.subtype)
+				input.attr("placeholder", unescapeHtml(field.placeholder))
+				input.attr("maxlength", field.maxlength) 
+				input.focusout(
+					function(){
+						if(field.required && $(this).val().length === 0 ){
+							alert("no valido");
+						}
+						else if (field.regex && webComponent.evaluateValueInRegex($(this).val(), field.regex) ){
+							alert("regex no valido");
+						}
+						else{
+							webComponent.isValidForm = true;
+							controller.responseFields.push({ idField: id, name: field.name, response: $(this).val() })
+						}				        
+					}
+				);
+				div.append(input);
+			}
+			return input;
+		};
+
+		function validateFieldsClass(item){
+			var re = /form-control/gi;
+			item.class = item.class.replace(re, "").split(" ").join(' ');
+			item.placeholder = item.placeholder || "";
+			//item.placeholder = unescapeHtml(item.placeholder);
+			if(item.required){
+				item.class = item.class.concat(" required");
+			}
+		};
+
+		function unescapeHtml(escapedStr) {
+			var div = document.createElement('div');
+			div.innerHTML = escapedStr;
+			var child = div.childNodes[0];
+			return child ? child.nodeValue : '';
+		};
+
+	},
+
+	evaluateValueInRegex: function(value,regex) {
+		var exp = new RegExp(b64_to_utf8( regex ));
+		return  exp.test(value);
+		function b64_to_utf8( str ) {
+			return decodeURIComponent(escape(window.atob( str )));
+		}
+	}
+
+
 }
