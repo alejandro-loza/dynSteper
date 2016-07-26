15cf7b6b21f0d7503ffad872a79a32aae63b0904
pinky <orochi_alex@hotmail.com>
2016-07-26 12:35:48 -0500

    estable validated text boxes

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 webComponent = {
-	formValues : [],	
-	responseFields: [],
+	_formValues : [],	
 	errorFields: [],
+	invalidFields : [],
 
-	_isValidForm: function(){
-       isFullRequired();
-  
-
-		function isFullRequired(){
-			var requiredFields ;
-			requiredFields = $.grep(webComponent.formValues, function(e){ return e.required === true; }); 
-        	$.each( requiredFields , function( index, field ) {
-				var search = $.grep(webComponent.responseFields, function(e){ return e.name === field.name; });
-				if(search.length === 0){
-					webComponent.errorFields.push(field);
-				}
-				else{
-					return true;	
-				}
-			});
-		};		
+    _getAllValues:function() {
+		var inputValues = [];
+		$('#container input').each(function() {
+			inputValues.push({ idField: $(this).attr("id"), name: $(this).attr("name")  , response: $(this).val() });
+		})
+		return inputValues;
 	},
 
-	main: function (entidad, tipoPago) {
+	_isValidForm: function(){
+
+		return isFullRequired() && isFullRegexValid();
+
+		function isFullRequired(){
+			webComponent.errorFields = [];
+			var requiredFields  = $.grep(webComponent._formValues, function(e){ return e.required === true; }); 
+			$.each( requiredFields , function( index, field ) {
+				var search = $.grep(webComponent._getAllValues(), function(e){ return e.name === field.name; });
+				alert("search:" + JSON.stringify(search));				
+				if(search.length === 0){
+					webComponent.errorFields.push(field);
+				}				
+			});
+			if(webComponent.errorFields.length !== 0){
+				$.each(webComponent.errorFields, function(index,field){
+					webComponent._addErrorClass(field.id);	
+				})
+				return false;
+			}
+			else{
+				return true;
+			}
+		};
+
+		function isFullRegexValid(){
+			webComponent.invalidFields = [];
+			var regexFields = $.grep(webComponent._formValues, function(e){ return e.regex });
+			$.each( regexFields , function( index, field ) {
+				var findField  = $.grep(webComponent._getAllValues(), function(el){ return el.name = field.name });
+				if(findField[0].response.length > 0 && !webComponent._evaluateValueInRegex(findField[0].response, field.regex)){
+					webComponent.invalidFields.push(findField);
+				} 				             
+			});
+			if(webComponent.invalidFields.length !== 0){
+				$.each(webComponent.errorFields, function(index,field){
+					webComponent._addErrorClass(field.id);	
+				})
+				return false;
+			}
+			else{
+				return true;
+			}
+		};
+	},
+
+	main: function (entidad, tipoPago) {	
 		var controller = this;
 		$.ajax({
 			//url: 'http://10.15.3.31:3000/vun/actas_nacimiento/findOne?filter={"where":{"id_estado":"'+entidad+'","id_tipo_pago":'+ tipoPago +'}}',
@@ -32,37 +67,38 @@ webComponent = {
 			contentType: 'application/json; charset=UTF-8;',
 		}).done(function(entityFields) {
 			if(entityFields.PagoEnLinea){
-				controller.formValues = entityFields.PagoEnLinea; 
+				controller._formValues = entityFields.PagoEnLinea; 
 			}
 			else if (entityFields.PagoReferenciado){
-				controller.formValues = entityFields.PagoReferenciado ;
+				controller._formValues = entityFields.PagoReferenciado ;
 			}
 			else{
 				alert("No existen campos")
 			}
-			render( controller.formValues );
+			//render( controller._formValues );
 		})
 		.fail(function(e) {
 			alert(JSON.stringify(e));
 		});
+	},
 
-		function render(entityFields, canvas){
-			
-			$.each( entityFields, function( index, field ) {
-				validateFieldsClass(field);
-				switch(field.type) {					
-					case "text":
-						var id = field.name + index;
-						var div = getOrCreateDiv(id, field.class);
-						getOrCreateLabel(div,id, field.label);
-						getOrCreateTextInput(div,id, field);
-						addHelperBlock(div);
-						break;
-					default: 
-						alert('Default case');
-				}
-			});
-		};
+	_render: function (){
+		$.each( webComponent._formValues, function( index, field ) {
+			validateFieldsClass(field);
+			switch(field.type) {					
+				case "text":
+				var id = field.name + index;
+				field["id"] = id;
+				var div = getOrCreateDiv(id, field.class);
+				getOrCreateLabel(div,id, field.label);
+				getOrCreateTextInput(div,id, field);
+				addHelperBlock(div);
+				break;
+				default: 
+				alert('Default case');
+			}
+		});
+
 
 		function getOrCreateDiv(id, clazz){
 			var div = $("#" + id + "div");
@@ -91,23 +127,21 @@ webComponent = {
 				input.attr("id", id )
 				input.addClass('form-control')
 				input.attr("type", field.subtype)
+				input.attr("name", field.name)
 				input.attr("placeholder", unescapeHtml(field.placeholder))
 				input.attr("maxlength", field.maxlength) 
-				input.focusout(
-					function(){
-						if(field.required && $(this).val().length === 0 ){
-							alert("requerido " + field.name);
-							addErrorClass(id);
-						}
-						else if (field.regex && webComponent.evaluateValueInRegex($(this).val(), field.regex) ){
-							alert("regex no valido");
-						}
-						else{
-							webComponent.isValidForm = true;
-							controller.responseFields.push({ idField: id, name: field.name, response: $(this).val() })
-						}				        
+				input.focusout(	function(){
+					if(field.required && $(this).val().length === 0 ){
+						webComponent._addErrorClass(id,"required");
 					}
-				);
+					else if (field.regex && $(this).val().length > 0 && !webComponent._evaluateValueInRegex($(this).val(), field.regex) ){
+						webComponent._addErrorClass(id,"invalid");
+					}
+					else{
+						webComponent._removeErrorClass(id);						
+						//webComponent.responseFields.push({ idField: id, name: field.name, response: $(this).val() })
+					}				        
+				});
 				div.append(input);
 			}
 			return input;
@@ -118,14 +152,8 @@ webComponent = {
 			div.append(helper);
 		};
 
-		function addErrorClass(fieldId){
-			var htmlInputField = $( '#'+fieldId );
-			htmlInputField.parent().addClass( 'has-error' );
-            $( '.help-block', htmlInputField.parent() ).html( "Verifique este campo" ).slideDown();
-		};	
-
-
 		function validateFieldsClass(item){
+
 			var re = /form-control/gi;
 			item.class = item.class.replace(re, "").split(" ").join(' ');
 			item.placeholder = item.placeholder || "";
@@ -133,6 +161,7 @@ webComponent = {
 			if(item.required){
 				item.class = item.class.concat(" required");
 			}
+			return item;
 		};
 
 		function unescapeHtml(escapedStr) {
@@ -144,13 +173,27 @@ webComponent = {
 
 	},
 
-	evaluateValueInRegex: function(value,regex) {
+	_evaluateValueInRegex: function(value,regex) {
 		var exp = new RegExp(b64_to_utf8( regex ));
 		return  exp.test(value);
 		function b64_to_utf8( str ) {
 			return decodeURIComponent(escape(window.atob( str )));
 		}
+	},
+
+	_addErrorClass : function (fieldId, failType){
+		var failMessage;
+		if(failType === "required") {failMessage = "Campo Requerido"}
+			else {failMessage = "Campo invalido"}
+				var htmlInputField = $( '#'+fieldId );	
+			htmlInputField.parent().addClass( 'has-error' );
+			$( '.help-block', htmlInputField.parent() ).html( failMessage).slideDown();
+		},
+		_removeErrorClass : function (fieldId){	
+			var htmlInputField = $( '#'+fieldId );	
+			htmlInputField.parent().removeClass( 'has-error' );
+			$( '.help-block', htmlInputField.parent()  ).slideUp().html( '' );
+		}
+
+
 	}
-
-
-}
